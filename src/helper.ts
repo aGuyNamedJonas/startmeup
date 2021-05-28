77d91028bad7591e999469dff3532af80b67128f
aGuyNamedJonas <hi@aGuyNamedJonas.com>
2021-05-28 09:46:15 +0300

    Add idea for startupme.config.json

--- src/helper.ts
+++ src/helper.ts
@@ -38,10 +38,11 @@ export function printUsage () {
   console.log(chalk.bold('Quickly download any github repo folder to jumpstart coding'))
   console.log('')
   console.log('Usage:')
-  console.log('$ npx startmeup repo [repoSubfolder] [localFolder]')
+  console.log('$ npx startmeup repo[:branch] [repoSubfolder] [localFolder]')
   console.log('')
   console.log('Options:')
   console.log('repo\t\tNeeds to be github.com/<user>/<subfolder>')
+  console.log('branch\t\t(Optional) Branch to use (default: main)')
   console.log('repoSubfolder\t(Optional) Repository subfolder (default: Entire repo)')
   console.log('localFolder\t(Optional) Local folder to download to (default: CWD)')
   console.log('')
@@ -50,3 +51,12 @@ export function printUsage () {
   console.log('https://github.com/aGuyNamedJonas/startmeup')
   console.log('')
 }
+
+/**
+ * (Optional) startmeup.config.json:
+ * {
+ *   "npm-install": true, // Runs npm install / yarn install after package is downloaded
+ *   "dependencies": {},  // Node dependencies to add (in case this is a partial starter meant for existing node.js projects)
+ *   "dev-dependencies": {}  // Node dev-dependencies to add (in case this is a partial starter meant for existing node.js projects)
+ * }
+ */
